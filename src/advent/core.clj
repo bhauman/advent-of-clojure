da46d605dce0e4a25dc10958acfc2adf5c70af66
Bruce Hauman <bhauman@gmail.com>
2015-12-10 08:19:53 -0500

    day 10

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -388,3 +388,13 @@ NOT y -> i"))))
   
 )
 
+;; day 10
+
+(defn count-encode [x]
+  (apply str
+         (mapcat 
+          (juxt count first)
+          (partition-by identity x))))
+
+;; part 1 and part 2
+(count (last (take 41 (iterate count-encode "1321131112"))))
