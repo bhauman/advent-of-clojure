484d14605014107015aed985ba3584c0e9d87f6a
Bruce Hauman <bhauman@gmail.com>
2015-12-14 21:14:25 -0500

    smaller cleaner day 14

--- src/advent/day14.clj
+++ src/advent/day14.clj
@@ -6,20 +6,13 @@
 (def prob14 (line-seq (io/reader (io/resource "prob14"))))
 
 (defn line-to-data [line]
-  (as-> line x
-    (string/replace x "." "")
-    (read-string (str "(" x ")"))
-    (let [n (first x)
-          [speed travel restt] (filter integer? x)]
-      {:nm n :speed speed :travel travel :restt restt})))
+  (->> line
+       (re-seq #"\d+")
+       (map read-string)))
 
 (def reindeer (map line-to-data prob14))
 
-(def test-reindeer
-  [{:nm 'Comet  :speed 14 :travel 10 :restt 127}
-   {:nm 'Dancer :speed 16 :travel 11 :restt 162}])
-
-(defn traveled [{:keys [speed travel restt]} seconds]
+(defn traveled [[speed travel restt] seconds]
   (let [period (+ travel restt)]
     (+ (* travel speed (int (/ seconds period)))
      (let [finalp (rem seconds period)]
@@ -31,16 +24,15 @@
 #_(reduce max (map #(traveled % 2503) reindeer))
 
 (defn winners [reindeer seconds]
-  (let [results (map
-                 #(assoc % :distance (traveled % seconds))
-                 reindeer)
-        winner-score (apply max (map :distance results))]
-    (map :nm
-         (filter #(= (:distance %) winner-score) results))))
+  (let [res (apply merge-with concat
+                   (map
+                    #(hash-map (traveled % seconds) [%])
+                    reindeer))]
+    (get res (apply max (keys res)))))
 
 ; part 2
 #_(reduce max
- (vals
-  (frequencies
-   (mapcat (partial winners reindeer)
-           (range 1 (inc 2503))))))
+          (vals
+           (frequencies
+            (mapcat (partial winners reindeer)
+                    (range 1 (inc 2503))))))
