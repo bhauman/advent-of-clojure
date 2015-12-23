f614b2e579c0e3aa92902cc35857abe5686257e0
Bruce Hauman <bhauman@gmail.com>
2015-12-23 08:18:09 -0600

    day 23

--- project.clj
+++ project.clj
@@ -7,6 +7,8 @@
                  [digest "1.4.4"]
                  [org.clojure/math.combinatorics "0.1.1"]
                  [org.clojure/core.match "0.2.2"]]
+
+  
   :profiles { :dev { :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}
               :repl { :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]
                       :repl-options {:init (set! *print-length* 500)}} })
