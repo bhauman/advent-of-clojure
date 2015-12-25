6c6bfb59fb056dee655cf4b36ddf72bd53e644f9
Bruce Hauman <bhauman@gmail.com>
2015-12-25 00:32:24 -0600

    Merry Christmas!

---
+++ src/advent/day25.clj
@@ -0,0 +1,19 @@
+(ns advent.day25)
+
+; Enter the code at row 2947, column 3029.
+
+(defn pos-from-row-col [r c]
+  (+ (nth
+      (reductions + (range))
+      (dec (+ c (dec r))))
+     c))
+
+(defn at-pos [pos]
+  (loop [x 20151125
+         c (dec pos)]
+    (if (zero? c)
+      x
+      (recur (rem (* x 252533) 33554393)
+             (dec c)))))
+
+(at-pos (pos-from-row-col 2947 3029))
